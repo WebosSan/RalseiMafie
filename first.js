e62a05ff251d63748c088156e51ce27407eeee40
WebosSan <88152488+WebosSan@users.noreply.github.com>
2022-11-01 09:02:37 -0500

    Update first.js

--- first.js
+++ first.js
@@ -1,2 +1,8 @@
-var xd = doument.getElementById('digite');
-xd.value = "aea";
+function onKeyDownHandler(evt) {
+    let inputValue1 = document.querySelector("#domTextElement1").value; 
+    var e = evt || window.event;        //Obtenemos el event
+    var key = evt.keyCode || evt.which; //Tecla presionada
+    if (key == 13 && inputValue1 == "Dont Let me Alone"){
+      console.log("acceso concedido");
+    }
+  }
