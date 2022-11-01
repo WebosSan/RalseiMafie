c7b153cd16df2cb6a55bdff5799d2c063a32fce2
WebosSan <88152488+WebosSan@users.noreply.github.com>
2022-11-01 09:33:55 -0500

    Update first.js

--- first.js
+++ first.js
@@ -1,8 +1,13 @@
+var body = document.getElementsByTagName("body")[0];
+var input = document.createElement("a");
+input.href = "yeah.html";
+
 function onKeyDownHandler(evt) {
     let inputValue1 = document.querySelector("#domTextElement1").value; 
     var e = evt || window.event;        //Obtenemos el event
     var key = evt.keyCode || evt.which; //Tecla presionada
     if (key == 13 && inputValue1 == "Dont Let me Alone"){
-      console.log("acceso concedido");
+        console.log("acceso concedido");
+        input.click();
     }
   }
